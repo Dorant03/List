fcb0be40bf2a217ee2e5de223c023cf99acb5f09
Mirroslava <31761632+Mirroslava@users.noreply.github.com>
2017-11-02 19:11:07 +0200

    Update and rename Header.h to Stack.h

---
+++ Stack.h
@@ -0,0 +1,20 @@
+//
+//  Header.h
+//  Stack
+//
+//  Created by Барабаш Мирослава on 02.11.17.
+//  Copyright © 2017 Барабаш Мирослава. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+template <typename T>
+class Stack
+{
+private:
+    T *stackPtr;
+    const int size;
+    int top;
+
+};
+#endif /* Header_h */
